bcef7cefde30bfc52aac61afee10ff3a861b8486
Aswanikumar <aswanikumar_v@outlook.com>
2022-02-11 07:21:36 +0530

    commit2

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         subtract()
         print("This is fom dev2 branch")
         print("commit1")
+        print("commit2")
     }
     
     func add(){
